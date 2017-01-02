8d931e97d0820c05d708d1b318eff3c4e6479394
Shashank Raghunath <shashank.raghunath15@gmail.com>
2017-01-02 20:58:18 +0530

    asd

--- src/main/java/org/shashank/library/Library3Application.java
+++ src/main/java/org/shashank/library/Library3Application.java
@@ -1,13 +1,9 @@
 package org.shashank.library;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 public class Library3Application {
-
+/*
 	public static void main(String[] args) {
 		SpringApplication.run(Library3Application.class, args);
 		
-	}
+	}*/
 }
