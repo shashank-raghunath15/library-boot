6528e54f940a8ed1a786931e2c4464aa04dfa5ff
Shashank Raghunath <shashank.raghunath15@gmail.com>
2017-01-03 00:53:39 +0530

    Rollback

--- src/main/java/org/shashank/library/Library3Application.java
+++ src/main/java/org/shashank/library/Library3Application.java
@@ -1,9 +1,13 @@
 package org.shashank.library;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Library3Application {
-/*
+
 	public static void main(String[] args) {
 		SpringApplication.run(Library3Application.class, args);
-		
-	}*/
+
+	}
 }
